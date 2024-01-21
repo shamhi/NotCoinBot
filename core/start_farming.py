b4d10afce2c826d05ee0f8d0414d43585eda260c
ShamHi <shomakakdela@gmail.com>
2024-01-21 11:18:11 +0300

    Update

--- core/start_farming.py
+++ core/start_farming.py
@@ -481,6 +481,7 @@ class Farming:
 
                                     logger.debug(f"{self.session_name} | Генерация нового Auth токена")
 
+                                    tg_web_data: str = await self.get_tg_web_data(session_proxy=session_proxy)
                                     access_token: str = await self.get_access_token(client=client,
                                                                                     tg_web_data=tg_web_data)
                                     client.headers['Authorization']: str = f'Bearer {access_token}'
