69c4eaee8c478e10a8b452c72d1e8c565ca8fcd1
ShamHi <shomakakdela@gmail.com>
2024-01-22 15:56:53 +0300

    Update config

--- data/config.py
+++ data/config.py
@@ -10,17 +10,23 @@ if isfile(path='settings.json'):
 else:
     config_data = {}
 
+
+
 API_ID: int = config_data.get('API_ID')
 API_HASH: str = config_data.get('API_HASH')
-MIN_AVAILABLE_COINS: int = 150
+
 MIN_CLICKS_COUNT: int = 1
+MIN_AVAILABLE_COINS: int = 150
+
 AUTO_BUY_ENERGY_BOOST: bool = False
 AUTO_BUY_SPEED_BOOST: bool = False
 AUTO_BUY_CLICK_BOOST: bool = False
+
 USE_PROXY_FROM_FILE: bool = False
+
 SLEEP_BETWEEN_CLICK: list[int] = [10, 15]
 SLEEP_BEFORE_BUY_MERGE: list[int] = [10, 15]
 SLEEP_BEFORE_ACTIVATE_FREE_BUFFS: list[int] = [10, 15]
 SLEEP_BEFORE_ACTIVATE_TURBO: list[int] = [10, 15]
 SLEEP_AFTER_FORBIDDEN_STATUS: int = 5
-SLEEP_WITH_MIN_COINS: int = 300
+SLEEP_WITH_MIN_COINS: int = 100
