7230196b2018f7aff74bb3bbceefbb36f75ff7b0
shamhi <shomakakdela@gmail.com>
2024-03-18 00:32:18 +0300

    🎄 New config

--- config/config.py
+++ config/config.py
@@ -1,38 +1,36 @@
-import json
-from os.path import isfile
-
-if isfile(path='settings.json'):
-    try:
-        with open('settings.json', 'r') as file:
-            config_data = json.load(file)
-    except:
-        config_data = {}
-else:
-    config_data = {}
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
+class Settings(BaseSettings):
+    model_config = SettingsConfigDict(env_file=".env", env_ignore_empty=True)
 
-API_ID: int = config_data.get('API_ID')
-API_HASH: str = config_data.get('API_HASH')
+    API_ID: int | str = ''
+    API_HASH: str = ''
 
-MIN_CLICKS_COUNT: int = 5
-MIN_AVAILABLE_COINS: int = 200
+    SERVER_HOST: str = "0.0.0.0"
+    SERVER_PORT: int = 63351
 
-AUTO_BUY_ENERGY_BOOST: bool = False
-MAX_ENERGY_BOOST: int = 10
-AUTO_BUY_SPEED_BOOST: bool = False
-MAX_SPEED_BOOST: int = 10
-AUTO_BUY_CLICK_BOOST: bool = False
-MAX_CLICK_BOOST: int = 10
+    MIN_CLICKS_COUNT: int = 5
+    MIN_AVAILABLE_COINS: int = 200
 
-SLEEP_BY_MIN_COINS: bool = True
+    AUTO_BUY_ENERGY_BOOST: bool = False
+    MAX_ENERGY_BOOST: int = 10
+    AUTO_BUY_SPEED_BOOST: bool = False
+    MAX_SPEED_BOOST: int = 10
+    AUTO_BUY_CLICK_BOOST: bool = False
+    MAX_CLICK_BOOST: int = 10
 
-USE_PROXY_FROM_FILE: bool = False
+    SLEEP_BY_MIN_COINS: bool = True
 
-SLEEP_BETWEEN_CLICK: list[int] = [10, 25]
-SLEEP_BEFORE_BUY_MERGE: list[int] = [5, 15]
-SLEEP_BEFORE_ACTIVATE_FREE_BUFFS: list[int] = [5, 15]
-SLEEP_BEFORE_ACTIVATE_TURBO: list[int] = [5, 15]
-SLEEP_AFTER_FORBIDDEN_STATUS: int = 5
-SLEEP_TO_UPDATE_USER_DATA = 60 * 5
-SLEEP_BY_MIN_COINS_TIME: int = 60 * 5
+    USE_PROXY_FROM_FILE: bool = False
+
+    SLEEP_BETWEEN_CLICK: list[int] = [10, 25]
+    SLEEP_BEFORE_BUY_MERGE: list[int] = [5, 15]
+    SLEEP_BEFORE_ACTIVATE_FREE_BUFFS: list[int] = [5, 15]
+    SLEEP_BEFORE_ACTIVATE_TURBO: list[int] = [5, 15]
+    SLEEP_AFTER_FORBIDDEN_STATUS: int = 5
+    SLEEP_TO_UPDATE_USER_DATA: int = 60 * 5
+    SLEEP_BY_MIN_COINS_TIME: int = 60 * 5
+
+
+settings = Settings()
