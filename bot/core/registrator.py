73d9dddcb8ccfd55528c117f907dfd6a9d3ff67d
shamhi <shomakakdela@gmail.com>
2024-03-18 00:28:11 +0300

    🧹Update registator

--- bot/core/registrator.py
+++ bot/core/registrator.py
@@ -1,31 +1,20 @@
-import json
-
 import pyrogram
 from loguru import logger
 
-from bot.config import config
+from config import settings
 
 
 async def create_sessions() -> None:
-    API_ID: int | str = config.API_ID
-    API_HASH: str = config.API_HASH
+    API_ID: int | str = settings.API_ID
+    API_HASH: str = settings.API_HASH
 
     if not API_ID or not API_HASH:
-        API_ID: int | str = input('\nВведите ваш API_ID (для выхода нажмите Enter): ')
-        API_HASH: str = input('\nВведите ваш API_HASH (для выхода нажмите Enter): ')
+        raise ValueError("API_ID and API_HASH not found in the .env file.")
 
     session_name: str = input('\nВведите название сессии (для выхода нажмите Enter): ')
 
-    if not session_name or not API_ID or not API_HASH:
-        return
-
-    config_data = {
-        'API_ID': API_ID,
-        'API_HASH': API_HASH
-    }
-
-    with open('settings.json', 'w') as file:
-        json.dump(config_data, file, indent=4, ensure_ascii=False)
+    if not session_name:
+        return None
 
     session: pyrogram.Client = pyrogram.Client(
         api_id=API_ID,
