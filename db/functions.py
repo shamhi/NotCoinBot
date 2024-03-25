292084406d17131c06dcfdd03dbb47d1d0affc94
shamhi <shomakakdela@gmail.com>
2024-03-25 21:45:02 +0300

    📎 Add MAX_BAD_STATUSES

---
+++ db/functions.py
@@ -0,0 +1,6 @@
+
+
+
+class Database:
+    ...
+    # TODO: Statistics
--- db/manager.py
+++
@@ -1,8 +0,0 @@
-import aiosqlite
-
-
-class Database:
-    def __init__(self):
-        self.db = aiosqlite.connect(database='db/statistics.db')
-
-    # TODO: Statistics
