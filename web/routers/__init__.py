be335f3c5a5c342d823f8042e345fefb242b65d5
shamhi <shomakakdela@gmail.com>
2024-03-26 00:01:16 +0300

    🎊 Lite version, where telethon sessions aren't supported

--- web/routers/__init__.py
+++
@@ -1,2 +0,0 @@
-from .frontend import front_router
-from .backend import api_router
--- web/utils/models.py
+++
@@ -1,7 +0,0 @@
-from typing import Literal
-
-from pydantic import BaseModel
-
-
-class ClickStatus(BaseModel):
-    status: Literal['on', 'off']
